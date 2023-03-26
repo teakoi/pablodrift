377a2b82788e123f2dda6b1e2ce84d5b508524dc
teakoi <128823915+teakoi@users.noreply.github.com>
2023-03-26 15:14:24 -0600

    Update main_game.ino

--- main_game.ino
+++ main_game.ino
@@ -237,29 +237,3 @@ void Enemy(){
 
 }
 
-
-void CheckCollision(){
-  int PlayerBottomLeft = PlayerY + carh;
-
-
-
-  //enemy on top:
-  if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-      PlayerY <= (EnemyY + truckh)) {
-      PlayerY = (EnemyY + truckh);}
-  //enemy below
-  else if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-      (PlayerY + carh) >= EnemyY) {
-      PlayerBottomLeft = EnemyY; }
-  //enemy on front from top
-  else if (PlayerY >= EnemyY &&
-          (PlayerY + carh)  >= (EnemyY + carh) &&
-          (EnemyX <= (PlayerX + carw))) {
-          PlayerX = PlayerX - 1; }
-  // enemy on front from below
-  else if (PlayerY >= EnemyY &&
-          (PlayerY + carh) <= (EnemyY + truckh) &&
-          (EnemyX <= (PlayerX + carw))) {
-          PlayerX = PlayerX -1; }
-}
-
