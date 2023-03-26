a877258d9e7f7b441818953ea00b6fd66f2c4795
teakoi <128823915+teakoi@users.noreply.github.com>
2023-03-26 15:14:52 -0600

    Update collision.ino

--- collision.ino
+++ collision.ino
@@ -1,21 +1,25 @@
 void CheckCollision(){
+  int PlayerBottomLeft = PlayerY + carh;
 
-//enemy on top:
-if (Player X <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-    PlayerY <= (EnemyY + truckh) {
-    PlayerY = (EnemyY + truckh);}
-//enemy below
-else if (Player X <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-    (PlayerY + carh) >= EnemyY) {
-    (PlayerY + carh) = EnemyY; }
-//enemy on front from top
-else if (PlayerY >= EnemyY &&
-        (PlayerY + carh)  >= (EnemyY + carh) &&
-        (EnemyX <= (PlayerX + carw) {
-        Player X = PlayerX - 1; }
-// enemy on front from below
-else if (Player Y >= EnemyY &&
-        (Player Y + carh) <= (EnemyY + truckh) &&
-        (Enemy X <= (PlayerX + carw) {
-        Player X = PlayerX -1; }
+
+
+  //enemy on top:
+  if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
+      PlayerY <= (EnemyY + truckh)) {
+      PlayerY = (EnemyY + truckh);}
+  //enemy below
+  else if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
+      (PlayerY + carh) >= EnemyY) {
+      PlayerBottomLeft = EnemyY; }
+  //enemy on front from top
+  else if (PlayerY >= EnemyY &&
+          (PlayerY + carh)  >= (EnemyY + carh) &&
+          (EnemyX <= (PlayerX + carw))) {
+          PlayerX = PlayerX - 1; }
+  // enemy on front from below
+  else if (PlayerY >= EnemyY &&
+          (PlayerY + carh) <= (EnemyY + truckh) &&
+          (EnemyX <= (PlayerX + carw))) {
+          PlayerX = PlayerX -1; }
 }
+
