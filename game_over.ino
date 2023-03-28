950493f395f5c5e778d4187b76443d8c7201c4a2
teakoi <128823915+teakoi@users.noreply.github.com>
2023-03-28 14:14:12 -0600

    Update game_over.ino

--- game_over.ino
+++ game_over.ino
@@ -4,8 +4,25 @@ void GameOver(){
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(6);
   tft.println("GAME OVER");
+  
+  tft.setCursor(26, 50);
+  tft.setTextSize(4);
+  tft.println("Press the Right button to try again")
+  tft.setCursor(26,60);
+  tft.println("Press the Left button to return to main menu");
+   
   finished=millis();
   elapsed=finished-start;
   lcd.setCursor(0,7);
   lcd.print( (elapsed)//1000);
+  
+  if (digitalRead (ButtonDOWN) == LOW){
+    MainMenuState = true;
+    GameOverState = false;
+  } 
+  
+  if (digitalRead(ButtonUP)==HIGH) {
+    GameState = true;
+    GameOverState = false;
+  }
 }
