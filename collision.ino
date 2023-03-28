d0930745c91eb1eea8d75c1b1285d5570a421d6a
teakoi <128823915+teakoi@users.noreply.github.com>
2023-03-28 13:41:01 -0600

    Update collision.ino

--- collision.ino
+++ collision.ino
@@ -1,25 +1,10 @@
-void CheckCollision(){
-  int PlayerBottomLeft = PlayerY + carh;
+boolean CheckCollision(){
 
-
-
-  //enemy on top:
-  if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-      PlayerY <= (EnemyY + truckh)) {
-      PlayerY = (EnemyY + truckh);}
-  //enemy below
-  else if (PlayerX <= EnemyX && (PlayerX +carw) <= (EnemyX + truckw) &&
-      (PlayerY + carh) >= EnemyY) {
-      PlayerBottomLeft = EnemyY; }
-  //enemy on front from top
-  else if (PlayerY >= EnemyY &&
-          (PlayerY + carh)  >= (EnemyY + carh) &&
-          (EnemyX <= (PlayerX + carw))) {
-          PlayerX = PlayerX - 1; }
-  // enemy on front from below
-  else if (PlayerY >= EnemyY &&
-          (PlayerY + carh) <= (EnemyY + truckh) &&
-          (EnemyX <= (PlayerX + carw))) {
-          PlayerX = PlayerX -1; }
+  boolean result = false
+  if ((EnemyX >= PlayerX && EnemyX <= (PlayerX +carw)) &&
+        (EnemyY >= (PlayerY-carh) && EnemyY <= (PlayerY))) {
+          result = true;
+        }
+  return result;
 }
 
